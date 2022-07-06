0121148c6a12f7dc12d1ffdab6d56f9272eac157
nanayawcodes <103840077+nanayawcodes@users.noreply.github.com>
2022-07-06 14:40:53 +0000

    on it

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const cutOffs = [
+const english = document.querySelector('.english');
+const coreMaths = document.querySelector('.core-maths');
+const science = document.querySelector('.science');
+const social = document.querySelector('.social');
+const eMaths = document.querySelector('.e-maths');
+const physics = document.querySelector('.physics');
+const chemistry = document.querySelector('.chemistry');
+const biology = document.querySelector('.biology');
+
+const subject = document.querySelectorAll('#subject');
+const calcButton = document.querySelector('#calculate');
+
+calcButton.addEventListener('click', () => { 
     
-]
+})
