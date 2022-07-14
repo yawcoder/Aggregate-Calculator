39ebee49d378dde1f9d39ed5ca8f82ca81f3bfaf
nanayawcodes <103840077+nanayawcodes@users.noreply.github.com>
2022-07-14 11:58:09 +0000

    trying to create a function for adding values

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ const biology = document.querySelector('.biology');
 
 const subject = document.querySelectorAll('#subject');
 const calcButton = document.querySelector('#calculate');
+const aggregate = document.querySelector('#aggregate');
 
+//click event and fucntion for adding values of selected grades
 calcButton.addEventListener('click', () => {
-    
-})
+
+     let englishGrade = english.options[english.selectedIndex].value;
+     let coreMathsGrade = coreMaths.options[coreMaths.selectedIndex].value;
+     let scienceGrade = science.options[science.selectedIndex].value;
+     let socialGrade = social.options[social.selectedIndex].value;
+     let eMathsGrade = eMaths.options[eMaths.selectedIndex].value;
+     let physicsGrade = physics.options[physics.selectedIndex].value;
+     let chemistryGrade = chemistry.options[chemistry.selectedIndex].value;
+     let biologyGrade = biology.options[biology.selectedIndex].value;
+
+     //console.log(chemistryGrade);
+
+     if(socialGrade < scienceGrade){
+          
+     }
+});
