3986a48b774fadc19c049e766a36435ecfeac8b0
nanayawcodes <103840077+nanayawcodes@users.noreply.github.com>
2022-07-14 17:14:16 +0000

    on it

--- app.js
+++ app.js
@@ -7,25 +7,28 @@ const physics = document.querySelector('.physics');
 const chemistry = document.querySelector('.chemistry');
 const biology = document.querySelector('.biology');
 
-const subject = document.querySelectorAll('#subject');
+//const subject = document.querySelectorAll('#subject');
 const calcButton = document.querySelector('#calculate');
 const aggregate = document.querySelector('#aggregate');
 
-//click event and fucntion for adding values of selected grades
+//click event for adding values of grades
 calcButton.addEventListener('click', () => {
-
-     let englishGrade = english.options[english.selectedIndex].value;
-     let coreMathsGrade = coreMaths.options[coreMaths.selectedIndex].value;
-     let scienceGrade = science.options[science.selectedIndex].value;
-     let socialGrade = social.options[social.selectedIndex].value;
-     let eMathsGrade = eMaths.options[eMaths.selectedIndex].value;
-     let physicsGrade = physics.options[physics.selectedIndex].value;
-     let chemistryGrade = chemistry.options[chemistry.selectedIndex].value;
-     let biologyGrade = biology.options[biology.selectedIndex].value;
-
-     //console.log(chemistryGrade);
-
-     if(socialGrade < scienceGrade){
-          
-     }
+     gradeValue
 });
+
+
+
+//Functions
+
+//function to retrieve the value of a selected option
+function gradeValue(subject){
+     let num = subject.options[subject.selectedIndex].value;
+     return num;
+}
+
+function sumCore(arr){
+     let sum = 0;
+     arr.forEach(element => {
+          
+     });
+}
